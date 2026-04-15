bfca333ac46ed3743ecc68733f5929584317a81b
ramakrishnalavu <srivenkatasivaramakrishna_lavu@srmap.edu.in>
2026-04-15 15:33:37 +0530

    #commit

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+    // baseURL: 'https://http://localhost:8080/api',
+  baseURL: 'https://rk-demo.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
